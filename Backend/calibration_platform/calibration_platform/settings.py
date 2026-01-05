32471e3f75ee0df46250d4494e32d1425ff051d3
Sarjan Shah <your-email@gmail.com>
2026-01-06 01:53:48 +0530

    Auto-create superuser for Render free tier

--- Backend/calibration_platform/calibration_platform/settings.py
+++ Backend/calibration_platform/calibration_platform/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.auth import get_user_model
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +143,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+def create_default_superuser():
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="admin123"
+        )
+
+if os.environ.get("RENDER"):
+    create_default_superuser()
